9d918ac55c5dea368e4901ecd760ca18c9956e45
Sergey Larin <Nemezis69RUS@rambler.ru>
2012-01-27 02:15:03 +0400

    Added a Pages controller

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = 'af28ca79044d7c40e5d3491320d4f707d1fef59c570d1ee3c6deaa98c49ee0a1e7bd06d780e226f5a2200ca4fa78412fba68615909dd4545d6a70a833cc97163'
